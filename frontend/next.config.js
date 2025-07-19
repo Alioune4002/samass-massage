1ada124035b54d2976341a2f347895f0b1d9a6ea
Alioune Seck <seemebyalioune@gmail.com>
2025-07-19 23:27:43 +0200

    Ajouter gunicorn à requirements.txt

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/:path*' : '/api/:path*',
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://samass-massage.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
