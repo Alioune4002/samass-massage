b7b42cc569c4018499d6c1d222f55ee242a7f1b6
Alioune Seck <seemebyalioune@gmail.com>
2025-10-08 00:57:58 +0200

    Désactiver rewrites pour tester

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://samass-massage.onrender.com/api/:path*',
-      },
-    ];
+    return [];
   },
 };
